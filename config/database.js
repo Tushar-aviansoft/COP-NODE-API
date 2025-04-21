36ce01144c4dd663425e59264e92079f7dd8e7c2
Tushar-aviansoft <tusharmahajan@aviansoft.in>
2025-04-21 16:27:58 +0530

    add pull min connection

--- config/database.js
+++ config/database.js
@@ -11,6 +11,10 @@ const db = knex({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   },
+  pool: {
+    min: 2,
+    max: 10
+  },
 });
 if (process.env.QUERY == "YES") {
   db.on("query", (queryData) => {
