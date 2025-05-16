7e00226648964e7e389ab78d55e0b7f2ba8df941
Vraj Patel <vrajpatel@aviansoft.in>
2025-05-16 10:07:06 +0530

    state

--- services/sitemap.services.js
+++ services/sitemap.services.js
@@ -10,26 +10,33 @@ const siteMap = async (model, variant) => {
 
         if (model) {
             const models = await db("cop_brands_ms")
-            .innerJoin("cop_models", "cop_brands_ms.brand_id", "cop_models.brand_id")
-            .select(
-                db.raw(`CONCAT('/', cop_brands_ms.slug, '-cars/', cop_models.slug) AS slug`)
-            )
-            .where("cop_brands_ms.status", 1)
-            .where("cop_models.status", 1);
+                .innerJoin("cop_models", "cop_brands_ms.brand_id", "cop_models.brand_id")
+                .select(
+                    db.raw(`CONCAT('/', cop_brands_ms.slug, '-cars/', cop_models.slug) AS slug`)
+                )
+                .where("cop_brands_ms.status", 1)
+                .where("cop_models.status", 1);
 
             result.models = models;
         }
-
         if (variant) {
             const variants = await db("cop_brands_ms")
                 .innerJoin("cop_models", "cop_brands_ms.brand_id", "cop_models.brand_id")
                 .innerJoin("cop_variants", "cop_models.model_id", "cop_variants.model_id")
+                .innerJoin("cop_fv", "cop_fv.variant_id", "cop_variants.variant_id")
+                .innerJoin("cop_features_ms", "cop_features_ms.feature_id", "cop_fv.feature_id")
                 .select(
-                    db.raw(`CONCAT('/', cop_brands_ms.slug, '-cars/', cop_models.slug, '/' , cop_variants.slug ) AS slug`)
+                    db.raw(`CONCAT(cop_brands_ms.slug, '-cars/', cop_models.slug, '/', cop_variants.slug) AS slug`)
                 )
                 .where("cop_brands_ms.status", 1)
                 .where("cop_models.status", 1)
                 .where("cop_variants.status", 1)
+                .groupBy(
+                    "cop_brands_ms.slug",
+                    "cop_models.slug",
+                    "cop_variants.slug"
+                );
+
             result.variants = variants;
         }
 
